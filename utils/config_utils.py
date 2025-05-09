943ffa93d1c7b17ad373d80f1cfc8104e58f9268
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-09 15:06:00 +0800

    Update config_utils.py

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -17,3 +17,20 @@ class ConfigurationUtils:
                 json.dump(data, file, indent=4)
             else:
                 file.write(data)
+
+    @staticmethod
+    def default_settings(file_path):
+        default_settings = {
+            "database": {
+                "host": "localhost",
+                "user": "root",
+                "password": "",
+                "database": "your-database-name"
+            },
+            "reports": {},
+            "appeals": {}
+        }
+        
+        with open(file_path, 'w') as f:
+            json.dump(default_settings, f, indent=4)
+        print(f"Default settings file created at {file_path}.")
