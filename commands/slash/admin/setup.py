e120b042a94de430113b2d4fd740747ecf72122b
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-09 21:21:00 +0800

    Update setup.py

--- commands/slash/admin/setup.py
+++ commands/slash/admin/setup.py
@@ -1,4 +1,4 @@
-import discord
+import discord, asyncio
 from discord import Interaction, app_commands
 from utils.config_utils import ConfigurationUtils
 from utils.modals.form_modal import FormView
@@ -12,8 +12,8 @@ def setup(bot):
             app_commands.Choice(name="appeals", value="appeals")
         ]
     )
-    @app_commands.describe(form_channel="Select the channel where the form embed will be sent", pending_channel="Select the pending channel to post entries")
-    async def setup(interaction: Interaction, type: app_commands.Choice[str], form_channel: discord.TextChannel, pending_channel: discord.TextChannel):
+    @app_commands.describe(form_channel="Select the channel where the form embed will be sent", pending_channel="Select the pending channel to post entries", public_appeals_channel="(Appeals only) Select the public channel where appeals will be posted")
+    async def setup(interaction: Interaction, type: app_commands.Choice[str], form_channel: discord.TextChannel, pending_channel: discord.TextChannel, public_appeals_channel: discord.TextChannel = None):
         if not interaction.user.guild_permissions.administrator:
             return await interaction.response.send_message(embed=discord.Embed(description="You do not have permission to use this command.", color=discord.Color.red()), ephemeral=True)
         
@@ -21,7 +21,8 @@ def setup(bot):
             settings = bot.settings
             settings[type.value] = {
                 "form_channel": form_channel.id,
-                f"pending_{type.value}_channel": pending_channel.id
+                f"pending_{type.value}_channel": pending_channel.id,
+                **({"public_appeals_channel": public_appeals_channel.id} if type.value == "appeals" and public_appeals_channel else {})
             }
 
             ConfigurationUtils.save_config(f"config/settings.{bot.bot_config.get('environment').lower()}.json", settings)
@@ -29,15 +30,18 @@ def setup(bot):
             await interaction.response.send_message(embed=success_embed)
 
             if type.value == "reports":
-                embed = discord.Embed(title="Game Report", description="To report a player for in-game misconduct, please press the button below. \n\nInclude as much evidence as possible (e.g., screenshots, videos) to support your report.", color=discord.Color.blue())
+                embed = discord.Embed(title="Game Report Form", description="To report a player for in-game misconduct, please press the button below. \n\nInclude as much evidence as possible (e.g., screenshots, videos) to support your report.", color=discord.Color.blue())
             elif type.value == "appeals":
-                embed = discord.Embed(title="Appeal Request", description="To appeal a punishment, please press the button below. \n\nProvide a clear explanation and any supporting evidence for your appeal.", color=discord.Color.blue())
+                embed = discord.Embed(title="Game Appeal Form", description="To make an appeal for in-game misconduct, please press the button below. \n\nProvide a clear explanation and any supporting evidence for your appeal.", color=discord.Color.blue())
             else:
                 return
             embed.set_footer(text="If the button does not work, please inform a Moderator.")
 
             form_view = FormView(bot=bot, form_type=type.value, db_connection=bot.db_connection)
             await form_channel.send(embed=embed, view=form_view)
+
+            await asyncio.sleep(5)
+            await (await interaction.original_response()).delete()
 
         except Exception as e:
             await bot.error_embed(interaction, f"An error occurred: {e}")
