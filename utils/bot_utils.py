a403a5441b48a6add87bfa47aa5d8a1edfb39b2e
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-10 10:20:09 +0800

    Update bot_utils.py

--- utils/bot_utils.py
+++ utils/bot_utils.py
@@ -1,10 +1,3 @@
 class BotFunctions:
     def __init__(self, bot):
         self.bot = bot
-
-    def sync_commands(self, guild_id: int, command_name: str) -> bool:
-        guild_id_str = str(guild_id)
-        if guild_id_str not in self.global_settings:
-            return False
-        command_states = self.global_settings[guild_id_str].get("command_states", {})
-        return command_states.get(command_name, False)
