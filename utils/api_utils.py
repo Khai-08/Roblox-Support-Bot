f7602a84a25c296f3201abe23c257ee2ef0b51be
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-10 09:55:10 +0800

    Update api_utils.py

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -5,19 +5,39 @@ load_dotenv()
 
 BASE_URL, API_KEY = os.getenv("API_BASE_URL"), os.getenv("API_KEY")
 
-async def fetch_user_data(user_id: int, endpoint: str) -> dict:
+#####################
+# Documentation API #
+#####################
+
+async def fetch_user_data_v2(user_id: int, endpoint: str) -> dict:
     async with aiohttp.ClientSession(headers={"x-api-key": API_KEY}) as session:
         async with session.get(f"{BASE_URL}/cloud/v2/users/{user_id}{endpoint}") as resp:
             return await resp.json()
 
-async def fetch_user_info(user_id: int) -> dict:
+async def fetch_user_info_v2(user_id: int) -> dict:
     try: 
-        data = await fetch_user_data(user_id, "")
+        data = await fetch_user_data_v2(user_id, "")
         return { "id": data["id"], "name": data["name"], "displayName": data["displayName"]}
     except KeyError: return None
 
-async def fetch_user_thumbnail(user_id: int) -> str:
+async def fetch_user_thumbnail_v2(user_id: int) -> str:
     try: 
-        data = await fetch_user_data(user_id, ":generateThumbnail")
+        data = await fetch_user_data_v2(user_id, ":generateThumbnail")
         return data.get("response", {}).get("imageUri", "")
     except KeyError: return None
+
+#####################
+# PUBLIC API'S #
+#####################
+
+async def fetch_user_info_v1(user_id: int) -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://users.roblox.com/v1/users/{user_id}") as resp:
+            data = await resp.json()
+            return { "id": data["id"], "name": data["name"], "displayName": data["displayName"]}
+        
+async def fetch_user_thumbnail_v1(user_id: int) -> str: 
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={user_id}&size=420x420&format=Png&isCircular=true") as resp:
+            data = await resp.json()
+            return data.get('data', [{}])[0].get('imageUrl', "")
