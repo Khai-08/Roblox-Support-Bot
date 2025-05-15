5e774655708fb4b9d2a9108d1068457fd276df9a
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-15 21:22:18 +0800

    Update report_form.py

--- utils/modals/report_form.py
+++ utils/modals/report_form.py
@@ -43,7 +43,7 @@ class ReportFormModal(Modal, title="Game Report Form"):
             result = cursor.fetchone()
             if result and result[0] == "Approved":
                 return await self.bot.error_embed(interaction, "This user has already been moderated. You cannot submit another appeal.")
-            elif result and result[0] not in ("Approved", "Rejected"):
+            elif result and result[0] != "Rejected":
                 return await self.bot.error_embed(interaction, "This user already has a pending or active report.")   
 
         try:
