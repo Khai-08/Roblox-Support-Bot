236cc681452962f2e2fecb05b567775fa76c90d8
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-15 19:48:00 +0800

    Update form_modal.py

--- utils/modals/form_modal.py
+++ utils/modals/form_modal.py
@@ -6,10 +6,9 @@ from utils.modals.appeal_form import AppealFormModal
 from utils.modals.report_form import ReportFormModal
 
 class FormActionView(View):
-    def __init__(self, bot, user, form_type, db_connection, name=None, ban_reason=None, appeal_reason=None, additional_info=None, report_id=None):
+    def __init__(self, bot, form_type, db_connection, name=None, ban_reason=None, appeal_reason=None, additional_info=None, report_id=None):
         super().__init__(timeout=None)
         self.bot = bot
-        self.user = user
         self.form_type = form_type
         self.db_connection = db_connection
         self.name = name
@@ -17,41 +16,51 @@ class FormActionView(View):
         self.appeal_reason = appeal_reason
         self.additional_info = additional_info
         self.report_id = report_id
-        self.message = None
 
     @discord.ui.button(label="Approve", style=discord.ButtonStyle.green, custom_id="approve_btn")
     async def approve(self, interaction: discord.Interaction, button: Button):
         if not interaction.user.guild_permissions.administrator:
             return await self.bot.error_embed(interaction, "You do not have permission to use this command.")
         
-        try:            
+        try:
+            channel_id = self.bot.settings.get("reports", {}).get("pending_reports_channel")
+            channel = interaction.guild.get_channel(channel_id)
+
             embed = interaction.message.embeds[0]
             form_type = embed.title.lower()
             report_id = int(embed.footer.text.split("RPT-")[-1])
             if "appeal" in form_type:
                 table = "game_appeals"
                 id_col = "appeal_id"
+                type = "appeal"
             else:
                 table = "game_reports"
                 id_col = "report_id"
+                type = "report"
 
             with self.db_connection.cursor() as cursor:
                 cursor.execute(f"UPDATE {table} SET status = %s WHERE {id_col} = %s", ("Approved", report_id))
+                cursor.execute(f"SELECT submitted_by FROM {table} WHERE {id_col} = %s", (report_id,))
+                user_id = cursor.fetchone()
+                cursor.execute(f"SELECT message_id FROM {table} WHERE {id_col} = %s", (report_id,))
+                message_id = cursor.fetchone()
             self.db_connection.commit()
 
             embed.set_field_at(-1, name="Status", value="✅ Approved", inline=False)
             view = ReportActionView(self.bot, interaction.user, db_connection=self.db_connection)
-            await self.message.edit(embed=embed, view=view)
+            message = await channel.fetch_message(message_id[0])
+            await message.edit(embed=embed, view=view)
             
-            dm_embed = discord.Embed(title=f"{self.form_type.capitalize()} Approved", description="Your game report has been reviewed and processed. The reported user has been moderated." if self.form_type == "report" else "Your appeal has been reviewed and approved. Your ban has been lifted.", color=discord.Color.green())
-            dm_embed.set_footer(text="This is an automated message.")
-            await self.user.send(embed=dm_embed)
+            user = await self.bot.fetch_user(user_id[0])
+            user_embed = discord.Embed(title=f"{type.capitalize()} Approved", description=f"Your game report `RPT-{report_id}` has been reviewed and processed. The reported user has been moderated." if type == "report" else "Your appeal has been reviewed and approved. Your ban has been lifted.", color=discord.Color.green())
+            user_embed.set_footer(text="This is an automated message.")
+            await user.send(embed=user_embed)
             await self.bot.success_embed(interaction, "Form has been approved and the user has been notified.")
 
-            if self.form_type == "appeal":
-                channel_id = self.bot.settings.get("appeals", {}).get("public_appeals_channel")
-                channel = interaction.guild.get_channel(channel_id) if channel_id else None
-                if channel:
+            if form_type == "appeal":
+                appeal_channel_id = self.bot.settings.get("appeals", {}).get("public_appeals_channel")
+                appeal_channel = interaction.guild.get_channel(appeal_channel_id)
+                if appeal_channel:
                     embed = discord.Embed(title="Ban Appeal Status Update", color=discord.Color.green())
                     embed.add_field(name="Roblox Username", value=self.name, inline=False)
                     embed.add_field(name="Ban Reason", value=self.ban_reason, inline=False)
@@ -60,7 +69,7 @@ class FormActionView(View):
                         embed.add_field(name="Additional Info", value=self.additional_info, inline=False)
                     embed.add_field(name="Status", value="✅ Approved", inline=False)
                     embed.set_footer(text=f"Submitted by {interaction.user}", icon_url=interaction.user.display_avatar.url)
-                    await channel.send(embed=embed)
+                    await appeal_channel.send(embed=embed)
 
         except Exception as e:
             await interaction.response.send_message(f"Error: {e}", ephemeral=True)
@@ -70,7 +79,7 @@ class FormActionView(View):
         if not interaction.user.guild_permissions.administrator:
             return await self.bot.error_embed(interaction, "You do not have permission to use this command.")
         
-        await interaction.response.send_modal(RejectReasonModal(bot=self.bot, user=self.user, form_type=self.form_type, db_connection=self.db_connection, message=self.message, name=self.name, ban_reason=self.ban_reason, appeal_reason=self.appeal_reason, additional_info=self.additional_info, report_id=self.report_id))
+        await interaction.response.send_modal(RejectReasonModal(bot=self.bot, db_connection=self.db_connection, name=self.name, ban_reason=self.ban_reason, appeal_reason=self.appeal_reason, additional_info=self.additional_info, report_id=self.report_id))
     
     
 class FormButton(discord.ui.Button):
